672feedd272c1eb50de7a1cbe49c59c30223534e
npy179 <npy179@gmail.com>
2017-09-08 07:17:00 -0400

    fist commit

---
+++ ex1.py
@@ -0,0 +1,9 @@
+import numpy as np
+#numpy: a package for manipulating large arrays and matrices of numeric data
+
+#Arrays
+
+#1:create arrays
+a = np.array([1, 3, 6, 8], dtype=float)
+type(a)
+print(a)
